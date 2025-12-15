f02ccc1dfb73e7ce6ef556101b527078f93f789b
TensoRag-Deni <denistanb05@gmail.com>
2025-12-15 11:08:28 +0530

    Updated Readme

--- tests/__init__.py
+++
@@ -1,4 +0,0 @@
-""
-Tests for the GNSS Error Prediction System.
-"""
-# This file makes the tests directory a Python package
--- tests/test_simple.py
+++
@@ -1,6 +0,0 @@
-def test_simple():
-    assert 1 + 1 == 2
-
-if __name__ == "__main__":
-    import pytest
-    pytest.main([__file__, "-v"])
